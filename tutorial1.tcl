ca01a2c9183ea98a754a9d2603fb8237b30e7b11
Akhilkumar Reddy Dhanireddy <60695157+dhanireddy10@users.noreply.github.com>
2021-11-30 11:11:29 +0100

    Update tutorial1.tcl

--- tutorial1.tcl
+++ tutorial1.tcl
@@ -154,7 +154,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 set obj [get_filesets sources_1]
 # Import local files from the original project
 set files [list \
- [file normalize "$origin_dir/../blinky.v" ]\
+ [file normalize "$origin_dir/../tutorial1/blinky.v" ]\
 ]
 set imported_files [import_files -fileset sources_1 $files]
 
